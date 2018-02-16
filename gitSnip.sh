4d9cf7f367c0a825e2e4eaf30ed7e7ee9155fb3e
Gina Robins <gsr909@gmail.com>
2018-02-16 14:18:14 -0500

    exercises day 2

--- gitSnip.sh
+++ gitSnip.sh
@@ -7,15 +7,16 @@
 #  To run this file, at the command line type:  ./gitSnip.sh
 #  Created by Gina Robins on 2/16/18.
 #  
-echo "# nameOfRepo" >> README.md #pipe repo name into readme.md
+#  create new repo on git.com and copy repo link to clipboard
 # run -->  git init within directory be put in repo
 # run -->  git remote add origin << path to repo-->htps://github.com/Gina909/jscript.git>>
 # run -->  git status
 # run -->  git Add -A
 # run -->  git commit -m ""
 # run -->  git push - u origin master
+echo "# nameOfRepo" >> README.md #pipe repo name into readme.md
 git init
 git add README.md
 git commit -m "first commit"
-git remote add origin https://github.com/Gina909/javaScript.git
+git remote add origin https://github.com/Gina909/
 git push -u origin master
